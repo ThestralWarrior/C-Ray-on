1b8058bb9aab6a1850acc9bb1e9371b482660779
ThestralWarrior <ashbroezio@outlook.com>
2024-08-25 17:53:30 +0530

    modified main

--- src/main.c
+++ src/main.c
@@ -18,12 +18,14 @@ int main() {
     };
     Vector3 light_direction = {-1.0f, 0.0f, 0.0f};
     Color sphere_color = {0.0f, 1.0f, 0.0f};
+    Color light_color = {1.0f, 1.0f, 1.0f};
+    Color ambient_color = {0.3f, 0.3f, 0.3f};
     printf("Listing the contents of the scene.\n");
     for(int i = 0; i < 3; i++) {
         printf("(Sphere) Center: %f %f %f, Radius: %f\n", scene.spheres[i].center.x, scene.spheres[i].center.y, scene.spheres[i].center.z, scene.spheres[i].radius);
     }
     printf("Starting rendering...\n");
-    render(width, height, camera, scene, 3, light_direction, sphere_color);
+    render(width, height, camera, scene, 3, light_direction, light_color, sphere_color, ambient_color);
     printf("Rendering complete. Image saved as result.ppm.\n");
     return 0;
 }
