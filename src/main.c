866612e49dc1a73a58964ff29649342cd99d7bfb
ThestralWarrior <ashbroezio@outlook.com>
2024-08-28 09:30:39 +0530

    modified main

--- src/main.c
+++ src/main.c
@@ -11,15 +11,17 @@ int main() {
     int width = 400;
     int height = 400;
 
-    Color sphere_color = (Color) {0.0f, 1.0f, 0.0f};
+    Color sphere_color1 = (Color) {0.0f, 1.0f, 0.0f};
+    Color sphere_color2 = (Color) {1.0f, 1.0f, 0.0f};
+    Color sphere_color3 = (Color) {0.0f, 1.0f, 1.0f};
     Color light_color = (Color) {1.0f, 1.0f, 1.0f};
     Color ambient_color = (Color) {0.3f, 0.3f, 0.3f};
 
     Camera camera = {{0, 0, 1}, {0, 0, -1}};
 
-    Sphere sphere1 = {(Vector3){0, 0, -10.0f}, 1};
-    Sphere sphere2 = {(Vector3){-3, 1, -6.0f}, 1};
-    Sphere sphere3 = {(Vector3){1, -1, -4.0f}, 1};
+    Sphere sphere1 = {(Vector3){0, 0, -10.0f}, 1, sphere_color1};
+    Sphere sphere2 = {(Vector3){-3, 1, -6.0f}, 1, sphere_color2};
+    Sphere sphere3 = {(Vector3){1, -1, -4.0f}, 1, sphere_color3};
 
     Scene scene = (Scene){
         width,
@@ -30,15 +32,15 @@ int main() {
 
     Vector3 light_direction = {-1.0f, 0.0f, 0.0f};
     
-
-    printf("Listing the contents of the scene.\n");
+    printf("Listing the contents of the scene before entering rendering pipeline -\n");
     for(int i = 0; i < 3; i++) {
         printf("(Sphere) Center: %f %f %f, Radius: %f, Colors: %f %f %f\n", scene.spheres[i].center.x, scene.spheres[i].center.y, scene.spheres[i].center.z, scene.spheres[i].radius);
     }
     printf("Starting rendering...\n");
     double start_time = omp_get_wtime();
-    render(scene, 3, light_direction, light_color, sphere_color, ambient_color);
+    render(scene, 3, light_direction, light_color, ambient_color);
     double end_time = omp_get_wtime();
     printf("Rendering finished... Time taken %lfs.\n", end_time - start_time);
     return 0;
 }
+
